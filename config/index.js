18df0313e7683428ed2eaafbf5692f7e3e6a79c2
Phillip Pastore <phillippastore@gmail.com>
2020-10-27 21:39:24 -0400

    Revert "update public path to /gwc/"
    This reverts commit 01610b5df9933f39c1b7bb9c3e5af0ad60330c67.

--- config/index.js
+++ config/index.js
@@ -50,7 +50,7 @@ module.exports = {
     // Paths
     assetsRoot: path.resolve(__dirname, '../dist'),
     assetsSubDirectory: 'static',
-    assetsPublicPath: '/gwc/',
+    assetsPublicPath: '/',
 
     /**
      * Source Maps
